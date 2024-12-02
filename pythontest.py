24c8fbbcf1e3326489bbc88396437cd44bd75075
neof1re <frost.box2024@gmail.com>
2024-12-02 04:16:22 +0300

    Add timer

--- pythontest.py
+++ pythontest.py
@@ -1,7 +1,16 @@
+import time
 
+def auto_stop_stopwatch(duration):
+    print("Секундомер запущен и остановится через 10 секунд.")
+    start_time = time.time()
+    end_time = start_time + duration
 
+    while time.time() < end_time:
+        elapsed_time = time.time() - start_time
+        print(f"Прошло времени: {int(elapsed_time)} секунд")
+        time.sleep(1)
 
-print("Hello World")
+    print(f"Секундомер остановлен. Прошло времени: {duration} секунд.")
 
-x = 3
-print(x)
+if __name__ == "__main__":
+    auto_stop_stopwatch(10)
